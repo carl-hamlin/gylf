60d9f6cd7769d83573650a8d086621b296688ba8
Carl Hamlin <hamlin.carlisle@gmx.com>
2011-10-15 09:59:56 -0600

    Code prettification project: FIxed headers.

--- code/commands/command.egg.asm
+++ code/commands/command.egg.asm
@@ -1,13 +1,13 @@
 ;================================================================================================================================================================================================
 ;
-;    command.egg.asm
+;   command.egg.asm
 ;
-;    This function creates a fertilized egg for a new user.
+;   This function creates a fertilized egg for a new user.
 ;
-;    Assumptions:   eax - Length of received data.
+;   Assumptions:    eax - Length of received data.
 ;                   esi - Pointer to descriptor associated with target socket.
 ;
-;    Returns:       None.
+;   Returns:        None.
 ;
 
 command.egg:    ret
