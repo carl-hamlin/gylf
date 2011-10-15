51f1e8500543da99b096211532d13dce4a47004e
Carl Hamlin <hamlin.carlisle@gmx.com>
2011-10-15 10:03:37 -0600

    Code prettification project.

--- code/commands/command.help.asm
+++ code/commands/command.help.asm
@@ -1,13 +1,13 @@
 ;================================================================================================================================================================================================
 ;
-;	command.help.asm
+;   command.help.asm
 ;
-;	This function displays context-sensitive help.
+;   This function displays context-sensitive help.
 ;
-;	Assumptions:	eax - Length of received data.
-;			esi - Pointer to descriptor associated with target socket.
+;   Assumptions:    eax - Length of received data.
+;                   esi - Pointer to descriptor associated with target socket.
 ;
-;	Returns:	None.
+;   Returns:        None.
 ;
 
-command.help:	ret
+command.help:   ret
