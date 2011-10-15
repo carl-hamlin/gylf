d3001eb5af10aba0455ec47f9b08352b125c256b
Carl Hamlin <hamlin.carlisle@gmx.com>
2011-10-15 13:37:04 -0600

    Code prettification project.

--- code/functions/check.admin.asm
+++ code/functions/check.admin.asm
@@ -1,18 +1,18 @@
 ;====================================================================================================================================================================================================
 ;
-;	check.admin
+;   check.admin
 ;
-;	This function checks to see if the current character is an admin.
+;   This function checks to see if the current character is an admin.
 ;
-;	Assumptions:	esi - Index to connection table.
+;   Assumptions:    esi - Index to connection table.
 ;
-;	Returns:	Carry - Admin
-;			No Carry - Not Admin
+;   Returns:        Carry - Admin
+;                   No Carry - Not Admin
 ;
 
-check.admin:	call	get.character.data		; Populate character data.
+check.admin:    call    get.character.data              ; Populate character data.
 
-		mov	al, byte [administrative.byte]	; al - Administrative byte.
-		shr	al, 01h				; Set carry if the least significant bit is high.
+                mov     al, byte [administrative.byte]  ; al - Administrative byte.
+                shr     al, 01h                         ; Set carry if the least significant bit is high.
 
-		ret					; Return to caller.
+                ret                                     ; Return to caller.
