8c0433d0294e0e6d798163f5f13e1cf1c008b8b0
Carl Hamlin <hamlin.carlisle@gmx.com>
2011-10-15 11:00:12 -0600

    Code prettification project.

--- code/functions/bail.asm
+++ code/functions/bail.asm
@@ -1,12 +1,12 @@
 ;====================================================================================================================================================================================================
 ;
-;	bail
+;   bail
 ;
-;	This function suspends execution and returns control back to linux.
+;   This function suspends execution and returns control back to linux.
 ;
-;	Assumptions:	None.
+;   Assumptions:    None.
 ;
-;	Returns:	None.
+;   Returns:        None.
 
-bail:	mov	eax, sys.exit.call	; eax = Suspend execution and return control to linux.
-	int	sys.system.call		; Call the kernel and let it know we're done here.
+bail:   mov     eax, sys.exit.call  ; eax = Suspend execution and return control to linux.
+        int     sys.system.call     ; Call the kernel and let it know we're done here.
