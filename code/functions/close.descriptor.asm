ad6d085ddcee5ebb02cc1873257b74dba459be55
Carl Hamlin <hamlin.carlisle@gmx.com>
2011-10-15 14:23:08 -0600

    Code prettification project.

--- code/functions/close.descriptor.asm
+++ code/functions/close.descriptor.asm
@@ -1,14 +1,14 @@
 ;====================================================================================================================================================================================================
 ;
-;	close.descriptor
+;   close.descriptor
 ;
-;	This function tells the linux kernel that we're done making use of an associated descriptor, and that it's time to disassociate it for later use.
+;   This function tells the linux kernel that we're done making use of an associated descriptor, and that it's time to disassociate it for later use.
 ;
-;	Assumptions:	ebx = Descriptor to be disassociated.
+;   Assumptions:    ebx = Descriptor to be disassociated.
 ;
-;	Returns:	eax = Error indicator.
+;   Returns:        eax = Error indicator.
 ;
 
-close.descriptor:	mov	eax, sys.close.call	; eax = Disassociate descriptor specified in ebx.
-			int	sys.system.call		; Call the kernel and have it perform the disassociation.
-			ret				; return to caller.
+close.descriptor:   mov     eax, sys.close.call ; eax = Disassociate descriptor specified in ebx.
+                    int     sys.system.call     ; Call the kernel and have it perform the disassociation.
+                    ret                         ; return to caller.
