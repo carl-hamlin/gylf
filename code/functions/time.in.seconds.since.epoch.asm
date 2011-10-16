d5af4f0fd4f28926699b0b1a2034adb9a2512110
Carl Hamlin <hamlin.carlisle@gmx.com>
2011-10-16 14:51:46 -0600

    Code prettification project.

--- code/functions/time.in.seconds.since.epoch.asm
+++ code/functions/time.in.seconds.since.epoch.asm
@@ -1,17 +1,17 @@
 ;====================================================================================================================================================================================================
 ;
-;	time.in.seconds.since.epoch
+;   time.in.seconds.since.epoch
 ;
-;	This function returns the time in seconds since the epoch.
+;   This function returns the time in seconds since the epoch.
 ;
-;	Assumptions:	ebx - null if results to be included in eax.
-;			ebx - pointer to buffer for results if results not only to be included in eax.
+;   Assumptions:    ebx - null if results to be included in eax.
+;                   ebx - pointer to buffer for results if results not only to be included in eax.
 ;
-;	Returns:	eax - time in seconds since the epoch.
-;			[ebx] - time in seconds since the epoch if function entered with non-null ebx.
+;   Returns:        eax - time in seconds since the epoch.
+;                   [ebx] - time in seconds since the epoch if function entered with non-null ebx.
 ;
 
-time.in.seconds.since.epoch:	mov	eax, sys.get.time.in.seconds.since.the.epoch	; eax - get time in seconds since the epoch.
-				int	sys.system.call					; Get time in seconds since the epoch.
+time.in.seconds.since.epoch:    mov     eax, sys.get.time.in.seconds.since.the.epoch    ; eax - get time in seconds since the epoch.
+                                int     sys.system.call                                 ; Get time in seconds since the epoch.
 
-				ret							; Return to caller.
+                                ret                                                     ; Return to caller.
