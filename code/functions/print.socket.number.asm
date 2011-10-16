0d93d3e8dc8dd84e730072a55999af0c1f726a8f
Carl Hamlin <hamlin.carlisle@gmx.com>
2011-10-16 14:22:31 -0600

    Code prettification project.

--- code/functions/print.socket.number.asm
+++ code/functions/print.socket.number.asm
@@ -1,21 +1,21 @@
 ;=================================================================================================================================================================================================
 ;
-;	print.socket.number
+;   print.socket.number
 ;
-;	This function prints a socket number for the admin.
+;   This function prints a socket number for the admin.
 ;
-;	Assumptions:	esi - Index to socket entry.
+;   Assumptions:    esi - Index to socket entry.
 ;
-;	Returns:	None.
+;   Returns:        None.
 ;
 
-print.socket.number:	pusha					; Preserve caller registers.
+print.socket.number:    pusha                                   ; Preserve caller registers.
 
-			mov	ecx, esi			; ecx - index to socket entry.
-			add	ecx, connection.number.index	; ecx - pointer to socket number.
-			mov	edx, dword.l			; edx - size of socket number.
-			call	write.descriptor		; Write the socket number locally.
+                        mov     ecx, esi                        ; ecx - index to socket entry.
+                        add     ecx, connection.number.index    ; ecx - pointer to socket number.
+                        mov     edx, dword.l                    ; edx - size of socket number.
+                        call    write.descriptor                ; Write the socket number locally.
 
-			popa					; Restore caller registers.
+                        popa                                    ; Restore caller registers.
 
-			ret					; Return to caller.
+                        ret                                     ; Return to caller.
