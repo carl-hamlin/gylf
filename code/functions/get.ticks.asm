a911179b7449951a82d94666a885f73b85383c95
Carl Hamlin <hamlin.carlisle@gmx.com>
2011-10-16 13:20:08 -0600

    Code prettification project.

--- code/functions/get.ticks.asm
+++ code/functions/get.ticks.asm
@@ -1,16 +1,16 @@
 ;====================================================================================================================================================================================================
 ;
-;	get.ticks
+;   get.ticks
 ;
-;	This function returns the number of clock ticks since boot time.
+;   This function returns the number of clock ticks since boot time.
 ;
-;	Assumptions:	ebx - null if results to be included in eax.
-;			ebx - pointer to buffer for results if results not only to be included in eax.
+;   Assumptions:    ebx - null if results to be included in eax.
+;                   ebx - pointer to buffer for results if results not only to be included in eax.
 ;
-;	Returns:	eax - number of clock ticks since boot time.
-;			[ebx] - number of clock ticks since boot time if function entered with non-null ebx.
+;   Returns:        eax - number of clock ticks since boot time.
+;                   [ebx] - number of clock ticks since boot time if function entered with non-null ebx.
 ;
 
-get.ticks:	mov	eax, sys.get.ticks	; eax - get number of ticks since boot time.
-		int	sys.system.call		; get the numberof ticks since boot time and put them in the eax register.
-		ret				; Return to caller.
+get.ticks:  mov     eax, sys.get.ticks  ; eax - get number of ticks since boot time.
+            int     sys.system.call     ; get the numberof ticks since boot time and put them in the eax register.
+            ret                         ; Return to caller.
