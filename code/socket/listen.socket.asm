085964b5fcf8f344336f271f97c09ad43944a832
Carl Hamlin <hamlin.carlisle@gmx.com>
2011-10-16 19:25:31 -0600

    Code prettification project.

--- code/socket/listen.socket.asm
+++ code/socket/listen.socket.asm
@@ -1,19 +1,19 @@
 ;====================================================================================================================================================================================================
 ;
-;	listen.socket
+;   listen.socket
 ;
-;	This function tells a created and bound socket to listen for incoming connections.
+;   This function tells a created and bound socket to listen for incoming connections.
 ;
-;	Assumptions:	None.
+;   Assumptions:    None.
 ;
-;	Returns:	eax = Error indicator.
+;   Returns:        eax = Error indicator.
 ;
 
-listen.socket:	mov	dword [socket.data.queue.l], sys.socket.queue.l	; Set the queue length.
+listen.socket:  mov     dword [socket.data.queue.l], sys.socket.queue.l ; Set the queue length.
 
-		mov	eax, sys.socket.call				; eax = Socket call indicator.
-		mov	ebx, sys.socket.listen				; ebx = Tell the socket to listen.
-		mov	ecx, socket.data.listening.socket.descriptor	; ecx = Pointer to arguments package for the listen function.
-		int	sys.system.call					; Tell the socket to listen.
+                mov     eax, sys.socket.call                            ; eax = Socket call indicator.
+                mov     ebx, sys.socket.listen                          ; ebx = Tell the socket to listen.
+                mov     ecx, socket.data.listening.socket.descriptor    ; ecx = Pointer to arguments package for the listen function.
+                int     sys.system.call                                 ; Tell the socket to listen.
 
-		ret							; Return to caller.
+                ret                                                     ; Return to caller.
