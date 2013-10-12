f4622a3f2be5f77ae8f5d8b5f8525f8f38142f21
Carl Hamlin <carl.hamlin@level3.com>
2013-10-12 02:50:52 -0600

    More strangeness in the command search loop. I'm beginning to wonder if these issues have to do with a fundamental change introduced by a new version of Ubuntu - this was all working two years ago.

--- code/player.interaction/address.active.status.asm
+++ code/player.interaction/address.active.status.asm
@@ -25,17 +25,14 @@ address.active.status:                      call    recv.from.socket            
 address.active.status.read.command.table:   push    ecx                                                                     ; Preserve loop counter.
                                             sub     ecx, ecx                                                                ; ecx - Prepared to be used as string length indicator.
                                             mov     cl, byte [esi]                                                          ; cl - length of command in table.
-                                            inc     esi                                                                     ; esi - Pointer to string data corresponding to command currently being
-                                                                                                                            ; referenced in the table.
+                                            inc     esi                                                                     ; esi - Pointer to string data corresponding to command currently being referenced in the table.
 
                                             cmp     cl, al                                                                  ; Is the length of the received data at least the same as the command in the table?
                                             ja      address.active.status.next.command                                      ; No. It's not necessary to perform any further comparison; the received data cannot possibly refer to the command currently being read from the table. Move on to the next table entry.
 
                                             repz    cmpsb                                                                   ; Check to see if the received data is the same as the string data corresponding to the command currently being referenced in the table.
-                                            jcxz    address.active.status.found.command                                     ; Did we get all the way through the string before cmpsb cut us loose? If
-                                                                                                                            ; so, go set up the handler and jump to it.
-
-                                            jmp     address.active.status.next.command.jcxnz                                ; Otherwise, go set up for the next iteration.
+                                            jnz     address.active.status.next.command.jcxnz                                ; Otherwise, go set up for the next iteration.
+                                            jcxz    address.active.status.found.command                                     ; Did we get all the way through the string before cmpsb cut us loose? If so, go set up the handler and jump to it.
 
 address.active.status.found.command:        pop     ecx                                                                     ; Restore loop counter.
                                             pop     edi                                                                     ; Restore caller's edi register...
