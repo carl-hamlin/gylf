6f987397d3b61e58290a641d5de2e4b0b9ee9420
Carl Hamlin <hamlin.carlisle@gmx.com>
2013-10-13 20:20:11 -0600

    Issue #18: Symbolic Cross-Referencing in Headers
    Added symbolic cross-referencing to code/commands/command.ai.asm.
    
    This is not expected to be the final commit for this issue.
    
    Files impacted by this commit:
    
    	code/commands/command.ai.asm

--- code/commands/command.ai.asm
+++ code/commands/command.ai.asm
@@ -1,13 +1,16 @@
 ;================================================================================================================================================================================================
 ;
-;   command.ai.asm
+; command.ai.asm
 ;
-;   This function sets parameters for the offline ai.
+; This function sets parameters for the offline ai.
 ;
-;   Assumptions:    eax - Length of received data.
-;                   esi - Pointer to descriptor associated with target socket.
+; Assumptions:    eax - Length of received data.
+;                 esi - Pointer to descriptor associated with target socket.
+; Returns:        None.
 ;
-;   Returns:        None.
+; Crossreferencing:	Symbol					Location
+;
+;			none					none
 ;
 
-command.ai:    ret
+	command.ai:    ret	; Return to caller
