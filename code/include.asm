b0d9d8a14a9794fdd8896af943334ee27829cb88
Carl Hamlin <hamlin.carlisle@gmx.com>
2017-02-08 14:02:55 -0800

    Issue #68: AI Command Reorganization Issue #81: Overall Include Rework
    The AI command now conforms to the new include standard.

--- code/include.asm
+++ code/include.asm
@@ -1,4 +1,4 @@
-%include "./code/commands/ai/code"
+%include "./code/commands/i"
 %include "./code/commands/command.area.asm"
 %include "./code/commands/command.broadcast.asm"
 %include "./code/commands/command.dig.asm"
